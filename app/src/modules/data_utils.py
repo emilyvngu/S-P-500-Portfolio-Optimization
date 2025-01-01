ecf27f99e5efdb275fdf2c806b72f639588b743d
emilyvngu <enguyen3838@gmail.com>
2024-12-31 21:03:52 -0500

    fixed filepaths

--- app/src/modules/data_utils.py
+++ app/src/modules/data_utils.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 def load_raw_data():
-    raw_data = pd.read_csv('src/assets/sp500_raw_data.csv', index_col=0, parse_dates=True)
+    raw_data = pd.read_csv('app/src/assets/sp500_raw_data.csv', index_col=0, parse_dates=True)
     return raw_data
 
 def load_mu_cov_data():
-    mu = pd.read_csv('src/assets/sp500_expected_returns.csv', index_col=0, squeeze=True)
-    cov_matrix = pd.read_csv('src/assets/sp500_covariance_matrix.csv', index_col=0)
+    mu = pd.read_csv('app/src/assets/sp500_expected_returns.csv', index_col=0, squeeze=True)
+    cov_matrix = pd.read_csv('app/src/assets/sp500_covariance_matrix.csv', index_col=0)
     return mu, cov_matrix
