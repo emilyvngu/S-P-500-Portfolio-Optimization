051eb5c4d2436e0e055ff2ad8112db2b1b3a7a3d
emilyvngu <enguyen3838@gmail.com>
2024-12-31 19:49:42 -0500

    deleted api

--- app/src/Home.py
+++ app/src/Home.py
@@ -23,3 +23,9 @@ Explore the sections below to see visualizations, results, and more!
 if st.sidebar.button("Get Random Ticker"):
     st.write("5")
 
+import nav
+page = nav.navigation()
+if page == "Visualization":
+    import pages.01_Portfolio_Visualization
+elif page == "Backtesting":
+    import pages.03_Backtesting
