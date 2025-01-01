83e893c3ff1e478ab6779ee219615f301e36702c
emilyvngu <enguyen3838@gmail.com>
2024-12-31 21:04:43 -0500

    fixed filepaths

--- app/src/modules/data_utils.py
+++ app/src/modules/data_utils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def load_raw_data():
-    raw_data = pd.read_csv('app/src/assets/sp500_raw_data.csv', index_col=0, parse_dates=True)
+    raw_data = pd.read_csv('/Users/emilynguyen/Desktop/SPY-Portfolio-Optimization/app/src/assets/sp500_raw_data.csv', index_col=0, parse_dates=True)
     return raw_data
 
 def load_mu_cov_data():
